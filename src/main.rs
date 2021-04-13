d6a222d10a641b40f5f0c124035ec7afb099db5f
Colin Cook <Colin.Cook@beyondidentity.com>
2021-04-13 07:04:17 -0500

    added enum method

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
     
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..];
+    // let string = String::from("127.0.0.1:8080");
+    // let string_slice = &string[10..];
+    // dbg!(&string);
+    // dbg!(string_slice);
 
-    dbg!(&string);
-    dbg!(string_slice);
+    // let get = Method::GET("abcd".to_string());
+    // let delete = Method::DELETE(100);
+    // let post = Method::POST;
+    // let put = Method::PUT;
     
-    // let server = Server::new("127.0.0.1:8080");
-    // server.run();
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
 
 struct Server {
@@ -22,6 +26,30 @@ impl Server {
         }
     }
     fn run(self) {
+        println!("Listening on {}", self.addr);
 
     }
 }
+
+struct Request {
+    path: String, 
+    query_string: String,
+    method: String,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATH,
+}
+
+
+// GET /user?id=10 HTTP/1.1\r\n
+// HEADERS \r\n
+// BODY
