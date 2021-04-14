453a9473c6e9ed70daaf44d55a8d7551b064c59d
Colin Cook <cook.colin13@gmail.com>
2021-04-14 05:28:04 -0500

    organised code into modules

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,14 @@
-fn main() {    
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod http;
+mod server;
+
+fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
-
-struct Server {
-    addr: String,
-
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr 
-        }
-    }
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-
-    }
-}
-
-struct Request {
-    path: String, 
-    query_string: Option<String>,
-    method: String,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATH,
-}
-
 
 // GET /user?id=10 HTTP/1.1\r\n
 // HEADERS \r\n
