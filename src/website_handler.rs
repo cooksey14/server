e7a6b6107d67c8d04aeaa2a630f383e84fcb6bc2
Colin Cook <cook.colin13@gmail.com>
2021-06-30 13:57:27 -0500

    serving an HTML file

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,5 +1,6 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
+use std::fs;
 
 pub struct WebSiteHandler {
     public_path: String
@@ -9,6 +10,11 @@ impl WebSiteHandler {
     pub fn new(public_path: String) -> Self {
         Self {public_path}
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
 }
 
 impl Handler for WebSiteHandler {
@@ -16,7 +22,7 @@ impl Handler for WebSiteHandler {
         Response::new(StatusCode::Ok, Some("<h1> TEST </h1>".to_string()));
             match request.method() {
                 Method::GET => match request.path() {
-                    "/" => Response::new(StatusCode::Ok, Some("<h1>Hello world!</h1>".to_string())),
+                    "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                     "/ahoy" => Response::new(StatusCode::Ok, Some("<h1>Ahoy there!</h1>".to_string())),
                     "/home" => Response::new(StatusCode::Ok, Some("<h1>Welcome home!</h1>".to_string())),
                     _ =>Response::new(StatusCode::NotFound, None),
